b4d8a4fee1236d64c3ac7aaf105c0053cceed94a
Jeff Miller <jmiller@pwiworks.com>
2024-12-12 11:37:33 -0500

    Revert "try with routeobserver"
    This reverts commit 9cf2646364d78bd0c5a21373c35a140e026b671b.

--- lib/auth_check.dart
+++ lib/auth_check.dart
@@ -8,9 +8,7 @@ import 'package:pwi_auth/utils.dart';
 import 'auth_check_view_model.dart';
 import 'login_page.dart';
 
-final RouteObserver<ModalRoute<void>> routeObserver = RouteObserver<ModalRoute<void>>();
-
-class AuthCheck extends ViewWidget<AuthCheckViewModel> with RouteAware {
+class AuthCheck extends ViewWidget<AuthCheckViewModel> {
   AuthCheck(
       {super.key,
       required String authenticatedRoute,
@@ -50,11 +48,20 @@ class AuthCheck extends ViewWidget<AuthCheckViewModel> with RouteAware {
         );
         break;
     }
+
+    viewModel.redirectLoopRunning = false;
   }
 
   @override
   Widget build(BuildContext context) {
     log("building AuthCheck view, redirectLoopRunning: ${viewModel.redirectLoopRunning}, authChecked: ${viewModel.authChecked}, isSignedIn: ${viewModel.isSignedIn}");
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!viewModel.redirectLoopRunning) {
+        viewModel.redirectLoopRunning = true;
+        _waitCheckAuth();
+      }
+    });
 
     return Scaffold(
       body: Stack(
@@ -90,21 +97,5 @@ class AuthCheck extends ViewWidget<AuthCheckViewModel> with RouteAware {
         ],
       ),
     );
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    routeObserver.subscribe(this, ModalRoute.of(context)!);
-    if (!viewModel.redirectLoopRunning) {
-      viewModel.redirectLoopRunning = true;
-      _waitCheckAuth();
-    }
-  }
-
-  @override
-  void didPopNext() {
-    // Called when this route is again visible after popping a next route
-    _waitCheckAuth();
   }
 }
