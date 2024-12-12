ed9e1a7bbbd9dd47dbcce9601ed3e58e3e6be78f
Jeff Miller <jmiller@pwiworks.com>
2024-12-12 11:55:38 -0500

    try entirely new approach

--- lib/auth_check.dart
+++ lib/auth_check.dart
@@ -20,46 +20,12 @@ class AuthCheck extends ViewWidget<AuthCheckViewModel> {
     if (!enableLogs) {
       enableLogs = loggingEnabled;
     }
-  }
-
-  void _waitCheckAuth() async {
-    if (!viewModel.authChecked) {
-      viewModel.waitAuthCheckLoops ++;
-      // wait for auth check to complete
-      await Future.delayed(const Duration(milliseconds: 300), _waitCheckAuth);
-      return;
-    }
-
-    switch (viewModel.isSignedIn) {
-      case true:
-        log("User is signed in, redirecting to ${viewModel.authenticatedRoute}");
-        Navigator.of(context)
-            .pushReplacementNamed(viewModel.authenticatedRoute);
-        break;
-      case false:
-        log("User is not signed in, redirecting to login page");
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => LoginPage(
-              appTitle: viewModel.appTitle,
-              authenticatedRoute: viewModel.authenticatedRoute,
-            ),
-          ),
-        );
-        break;
-    }
+    _waitCheckAuth();
   }
 
   @override
   Widget build(BuildContext context) {
     log("building AuthCheck view, redirectLoopRunning: ${viewModel.redirectLoopRunning}, authChecked: ${viewModel.authChecked}, isSignedIn: ${viewModel.isSignedIn}");
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (!viewModel.redirectLoopRunning) {
-        viewModel.redirectLoopRunning = true;
-        _waitCheckAuth();
-      }
-    });
 
     return Scaffold(
       body: Stack(
@@ -95,5 +61,39 @@ class AuthCheck extends ViewWidget<AuthCheckViewModel> {
         ],
       ),
     );
+  }
+
+  void _waitCheckAuth() async {
+    viewModel.redirectLoopRunning = true;
+    viewModel.waitAuthCheckLoops++;
+
+    while (!viewModel.authChecked) {
+      await Future.delayed(const Duration(milliseconds: 300));
+      viewModel.waitAuthCheckLoops++;
+    }
+
+    if (!mounted) {
+      viewModel.redirectLoopRunning = false;
+      return;
+    }
+
+    switch (viewModel.isSignedIn) {
+      case true:
+        log("User is signed in, redirecting to ${viewModel.authenticatedRoute}");
+        Navigator.of(context)
+            .pushReplacementNamed(viewModel.authenticatedRoute);
+        break;
+      case false:
+        log("User is not signed in, redirecting to login page");
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => LoginPage(
+              appTitle: viewModel.appTitle,
+              authenticatedRoute: viewModel.authenticatedRoute,
+            ),
+          ),
+        );
+        break;
+    }
   }
 }
