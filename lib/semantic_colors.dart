b324e6d3fde3a29856c6584937689b8a24f9005b
Jeff Miller <jmiller@pwiworks.com>
2025-01-10 11:45:57 -0500

    add semantic colors

---
+++ lib/semantic_colors.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+/// A class that defines a set of semantic colors used in the application.
+class SemanticColors {
+  /// The color used for warnings.
+  static const Color warning = Color(0xFFFFC800);
+
+  /// The color used for text/icons on warning backgrounds.
+  static const Color onWarning = Color(0xFFFFFFFF);
+
+  /// The color used for red seniority levels.
+  static const Color redSeniority = Color(0xFFF59689);
+
+  /// The color used for text/icons on red seniority backgrounds.
+  static const Color onRedSeniority = Color(0xFF2C2221);
+
+  /// The color used for green seniority levels.
+  static const Color greenSeniority = Color(0xFFABF589);
+
+  /// The color used for text/icons on green seniority backgrounds.
+  static const Color onGreenSeniority = Color(0xFF242C21);
+
+  /// The color used for blue seniority levels.
+  static const Color blueSeniority = Color(0xFF89CAF5);
+
+  /// The color used for text/icons on blue seniority backgrounds.
+  static const Color onBlueSeniority = Color(0xFF21272C);
+
+  /// The color used for orange seniority levels.
+  static const Color orangeSeniority = Color(0xFFF2AD5A);
+
+  /// The color used for text/icons on orange seniority backgrounds.
+  static const Color onOrangeSeniority = Color(0xFF2C2721);
+
+  /// The color used for yellow seniority levels.
+  static const Color yellowSeniority = Color(0xFFF2E55A);
+
+  /// The color used for text/icons on yellow seniority backgrounds.
+  static const Color onYellowSeniority = Color(0xFF2C2B21);
+}
