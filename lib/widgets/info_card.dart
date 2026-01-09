7763f69e79da6279d110829780f7d593b6c06048
deepalinagwade04 <dnagwade@pwiworks.com>
2026-01-09 09:10:12 -0500

    accept a link to display a rich text with a link

--- lib/widgets/info_card.dart
+++ lib/widgets/info_card.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:pwi_auth/semantic_colors.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 /// Enum representing the different display types for the [InfoCard].
 enum InfoCardDisplayType {
@@ -49,22 +51,31 @@ class InfoCard extends StatelessWidget {
   /// Optional override for icon to display alongside the message.
   final Icon? icon;
 
+  /// Optional link text to display after the message.
+  final String? link;
+
   /// Creates an [InfoCard] widget.
   ///
   /// The [message] parameter is required. The [useStandardCardMargin] parameter defaults
   /// to false, and the [displayType] parameter defaults to [InfoCardDisplayType.normal].
   /// The [icon] parameter is optional and can be used to override the default icon. If not provided,
   /// the icon will be determined based on the [displayType].
+  /// If [link] is provided, it is appended to the message as a clickable hyperlink.
   const InfoCard({
     super.key,
     required this.message,
     this.useStandardCardMargin = false,
     this.displayType = InfoCardDisplayType.normal,
     this.icon,
+    this.link,
   });
 
   @override
   Widget build(BuildContext context) {
+    final trimmedLink = link?.trim();
+    final hasLink = trimmedLink != null && trimmedLink.isNotEmpty;
+    final textColor = _getTextColor(context);
+
     return Card(
       margin: useStandardCardMargin ? null : EdgeInsets.zero,
       elevation: 0,
@@ -81,12 +92,27 @@ class InfoCard extends StatelessWidget {
             ),
             const SizedBox(width: 8),
             Flexible(
-              child: Text(
-                message,
-                style: TextStyle(
-                  color: _getTextColor(context),
-                ),
-              ),
+              child: hasLink
+                  ? RichText(
+                      text: TextSpan(
+                        style: TextStyle(color: textColor),
+                        children: [
+                          TextSpan(text: message),
+                          TextSpan(
+                            text: ' $trimmedLink',
+                            style: const TextStyle(
+                              decoration: TextDecoration.underline,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () => launchUrl(Uri.parse(trimmedLink)),
+                          ),
+                        ],
+                      ),
+                    )
+                  : Text(
+                      message,
+                      style: TextStyle(color: textColor),
+                    ),
             ),
           ],
         ),
